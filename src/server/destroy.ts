b7b84035382fdcc9929ad614f77df60b326ddbe0
Emmanuel Mahuni <emahuni@gmail.com>
2022-09-07 11:15:31 +0200

    fix(plugin/server): fix cleanup trigger

--- src/server/destroy.ts
+++ src/server/destroy.ts
@@ -1,11 +1,11 @@
 import type { Strapi } from '@strapi/strapi';
-import { name, uid } from '../../pluginId';
+import { name, id } from '../../pluginId';
 import chalk from 'chalk';
 
 export default async ({ strapi }: { strapi: Strapi }) => {
   strapi.log.info(chalk`{dim [destroy/()]-6:} destroying "${name}"...`);
   if (process.env.NODE_ENV === 'test') {
-    if (strapi.plugins[uid].config('cleanDBAtDestroy')) {
+    if (strapi.plugins[id].config('cleanDBAtDestroy')) {
       await strapi.services['plugin::vitest.utils'].cleanDB();
     }
   }
--- src/server/register.ts
+++ src/server/register.ts
@@ -1,11 +1,11 @@
 import type { Strapi } from '@strapi/strapi';
-import { uid, name } from '../../pluginId';
+import { name, id } from '../../pluginId';
 import chalk from 'chalk';
 
 export default async ({ strapi }: { strapi: Strapi }) => {
   strapi.log.info(chalk`{dim [register/()]-6:} registering "${name}"...`);
   if (process.env.NODE_ENV === 'test') {
-    if (strapi.plugins[uid].config('cleanDBAtRegister')) {
+    if (strapi.plugins[id].config('cleanDBAtRegister')) {
       await strapi.services['plugin::vitest.utils'].cleanDB();
     }
   }
