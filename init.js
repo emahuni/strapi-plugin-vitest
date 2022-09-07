eb628dd0cdf75b33cdb733c854533a448cca46bf
Emmanuel Mahuni <emahuni@gmail.com>
2022-09-07 15:08:48 +0200

    perf(tests/harness): reorganize constants & add harness test app dir into paths

--- init.js
+++ init.js
@@ -41,11 +41,11 @@ async function checkIfExistsAndCopy (checkIfThisExists, sourcePath, targetPath) 
 
 async function initTestHarness () {
   log(`Copying & overwriting testing harness helpers: \n\t-to: ${paths.TEST_DIR_HARNESS_PATH}\n`);
-  await fse.copy(paths.HARNESS_PATH, paths.TEST_DIR_HARNESS_PATH);
+  await fse.copy(paths.PLUGIN_HARNESS_PATH, paths.TEST_DIR_HARNESS_PATH);
   
   await checkIfExistsAndCopy(
       paths.APP_TEST_PATH,
-      paths.HARNESS_APP_TESTS_PATH,
+      paths.PLUGIN_HARNESS_APP_TESTS_PATH,
       paths.APP_TEST_PATH,
   );
   
@@ -57,13 +57,13 @@ async function initTestHarness () {
   
   await checkIfExistsAndCopy(
       paths.VITEST_CONFIG_FILE,
-      paths.HARNESS_VITEST_CONFIG_FILE,
+      paths.PLUGIN_HARNESS_VITEST_CONFIG_FILE,
       paths.VITEST_CONFIG_FILE,
   );
   
   await checkIfExistsAndCopy(
       paths.TEST_ENV_DB_CONFIG_FILE,
-      paths.HARNESS_DB_CONFIG_FILE,
+      paths.PLUGIN_HARNESS_DB_CONFIG_FILE,
       paths.TEST_ENV_DB_CONFIG_FILE,
   );
   
--- paths.js
+++ paths.js
@@ -2,37 +2,43 @@ const { existsSync } = require('fs-extra');
 const { resolve } = require('path');
 
 const PWD = process.env.PWD;
-
 const PLUGIN_DIR_PATH = __dirname;
-const HARNESS_TESTS_PATH = resolve(PLUGIN_DIR_PATH, 'harness/tests');
-const TEST_DIR_PATH = resolve(PWD, 'tests/');
 const isTS = existsSync(resolve(PWD, 'src/'));
+
+const TEST_DIR_PATH = resolve(PWD, 'tests/');
+const PLUGIN_HARNESS_TESTS_PATH = resolve(PLUGIN_DIR_PATH, 'harness/tests');
+
 const TEST_ENV_DB_CONFIG_DIR = resolve(PWD, `config/env/test/`);
 const TEST_ENV_DB_CONFIG_FILE = resolve(TEST_ENV_DB_CONFIG_DIR, `database.${isTS ? 'ts' : 'js'}`);
-const HARNESS_DB_CONFIG_FILE = resolve(PLUGIN_DIR_PATH, `harness/database.${isTS ? 'ts' : 'js'}`);
-const HARNESS_VITEST_CONFIG_FILE = resolve(PLUGIN_DIR_PATH, `harness/vitest.config.js`);
+const PLUGIN_HARNESS_DB_CONFIG_FILE = resolve(PLUGIN_DIR_PATH, `harness/database.${isTS ? 'ts' : 'js'}`);
+
 const VITEST_CONFIG_FILE = resolve(PWD, `vitest.config.js`);
+const PLUGIN_HARNESS_VITEST_CONFIG_FILE = resolve(PLUGIN_DIR_PATH, `harness/vitest.config.js`);
 
 const TEST_DIR_HARNESS_PATH = resolve(TEST_DIR_PATH, 'helpers/harness');
-const HARNESS_PATH = resolve(HARNESS_TESTS_PATH, 'helpers/harness');
-const HARNESS_APP_TESTS_PATH = resolve(HARNESS_TESTS_PATH, 'app.test.ts');
 const APP_TEST_PATH = resolve(TEST_DIR_PATH, 'app.test.ts');
+const PLUGIN_HARNESS_PATH = resolve(PLUGIN_HARNESS_TESTS_PATH, 'helpers/harness');
+const PLUGIN_HARNESS_APP_TESTS_PATH = resolve(PLUGIN_HARNESS_TESTS_PATH, 'app.test.ts');
 
 const TEST_APP_DIR_PATH = resolve(TEST_DIR_HARNESS_PATH, 'test-app');
+const PLUGIN_HARNESS_TEST_APP_DIR = resolve(PLUGIN_HARNESS_TESTS_PATH, 'helpers/test-app');
 
 module.exports = {
   PWD, isTS,
+  
   TEST_ENV_DB_CONFIG_DIR,
   TEST_ENV_DB_CONFIG_FILE,
-  HARNESS_TESTS_PATH,
-  HARNESS_DB_CONFIG_FILE,
   TEST_DIR_PATH,
-  PLUGIN_DIR_PATH,
-  HARNESS_VITEST_CONFIG_FILE,
   VITEST_CONFIG_FILE,
   TEST_DIR_HARNESS_PATH,
-  HARNESS_PATH,
-  HARNESS_APP_TESTS_PATH,
   APP_TEST_PATH,
-  TEST_APP_DIR_PATH
+  TEST_APP_DIR_PATH,
+  
+  PLUGIN_HARNESS_TESTS_PATH,
+  PLUGIN_HARNESS_DB_CONFIG_FILE,
+  PLUGIN_DIR_PATH,
+  PLUGIN_HARNESS_VITEST_CONFIG_FILE,
+  PLUGIN_HARNESS_PATH,
+  PLUGIN_HARNESS_APP_TESTS_PATH,
+  PLUGIN_HARNESS_TEST_APP_DIR
 };
