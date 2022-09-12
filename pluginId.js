92af474d7e26aa9f144c81e10eac81366e8fd583
Emmanuel Mahuni <emahuni@gmail.com>
2022-09-12 06:40:27 +0200

    perf: remove pluginId.js

--- pluginId.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-
-module.exports.name = path.basename(__dirname);
-module.exports.id = module.exports.name.replace(/^(@.*|strapi-)plugin-/i, '');
-module.exports.uid = `plugin::${module.exports.id}`;
