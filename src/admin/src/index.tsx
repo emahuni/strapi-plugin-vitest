5aa02e412a4ad19c2cb66126bfd86a749e9db7a2
Emmanuel Mahuni <emahuni@gmail.com>
2022-09-15 08:14:07 +0200

    fix(admin): fix admin issues by removing all admin code since we don't use it

--- src/admin/src/index.tsx
+++ src/admin/src/index.tsx
@@ -1,66 +1,6 @@
-import React from 'react';
-import { prefixPluginTranslations } from '@strapi/helper-plugin';
-import Initializer from './components/Initializer';
-import PluginIcon from './components/PluginIcon';
-
-import { packageInfo } from '@emanimation/strapi-utils';
-
-const pluginInfo = packageInfo(__dirname);
-
 export default {
-  register (app) {
-    app.addMenuLink({
-      to:          `/plugins/${pluginInfo.id}`,
-      icon:        PluginIcon,
-      intlLabel:   {
-        id:             `${pluginInfo.id}.plugin.name`,
-        defaultMessage: pluginInfo.name,
-      },
-      Component:   async () => {
-        const component = await import(/* webpackChunkName: "[request]" */ './pages/App');
-        
-        return component;
-      },
-      permissions: [
-        // Uncomment to set the permissions of the plugin here
-        // {
-        //   action: '', // the action name should be plugin::plugin-name.actionType
-        //   subject: null,
-        // },
-      ],
-    });
-    const plugin = {
-      id:          pluginInfo.id,
-      initializer: Initializer,
-      isReady:     false,
-      name:        pluginInfo.name,
-    };
-    
-    app.registerPlugin(plugin);
-  },
-  
+  register (app) {},
   bootstrap (app) {},
-  async registerTrads (app) {
-    const { locales } = app;
-    
-    const importedTrads = await Promise.all(
-        locales.map(locale => {
-          return import(`./translations/${locale}.json`)
-              .then(({ default: data }) => {
-                return {
-                  data: prefixPluginTranslations(data, pluginInfo.id),
-                  locale,
-                };
-              })
-              .catch(() => {
-                return {
-                  data: {},
-                  locale,
-                };
-              });
-        }),
-    );
-    
-    return Promise.resolve(importedTrads);
-  },
+  destroy (app) {},
+  registerTrads (app) {},
 };
--- src/admin/src/utils/getTrad.ts
+++
@@ -1,7 +0,0 @@
-import { packageInfo } from '@emanimation/strapi-utils';
-
-const pluginInfo = packageInfo(__dirname);
-
-const getTrad = (id: string) => `${pluginInfo.id}.${id}`;
-
-export default getTrad;
